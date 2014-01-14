a95038584af3edb62562014b240d10ca13df6975
raulg <raulg@google.com>
2014-01-14 08:10:12 -0800

    Added somemore of the example.

--- hola_mundo/el_grito.py
+++ hola_mundo/el_grito.py
@@ -1,2 +1,16 @@
+import os
+import random
+
 __author__ = 'Raul Gonzalez'
 
+
+class ElGrito(object):
+    def __init__(self):
+        current_dir = os.path.dirname(__file__)
+        grito_file = os.path.join(current_dir, 'resources', 'grito.txt')
+
+        with open(grito_file) as f:
+            self._gritos = f.readlines()
+
+    def grito(self):
+        return self._gritos[random.randint(0, len(self._gritos) - 1)]
