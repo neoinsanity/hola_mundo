93092dd8b9e036f6b21ed7471bab083ad8b4b309
raulg <raulg@google.com>
2014-01-14 16:43:13 -0800

    Finished up the sample code. Added the MANIFEST.in file for setuptools configuration.

--- hola_mundo/el_grito.py
+++ hola_mundo/el_grito.py
@@ -1,16 +1,34 @@
+"""Random salutation generator..
+
+"""
 import os
 import random
 
-__author__ = 'Raul Gonzalez'
-
 
 class ElGrito(object):
+    """Class that produces a random language version of 'hola'."""
+
     def __init__(self):
+        """Initialize ElGrito from a resource file.
+
+        :return: Nothing is returned.
+        :rtype: None
+        """
         current_dir = os.path.dirname(__file__)
         grito_file = os.path.join(current_dir, 'resources', 'grito.txt')
 
         with open(grito_file) as f:
             self._gritos = f.readlines()
 
-    def grito(self):
-        return self._gritos[random.randint(0, len(self._gritos) - 1)]
+    def grito(self, index=None):
+        """Method to retrieve a language version of 'hola'.
+
+        :param index: An index key to retrieve a given 'hola' translation.
+        :type index: int
+        :return: A language version of 'hola'.
+        :rtype: str
+        """
+        if index is not None:
+            return self._mundo[index]
+        else:
+            return self._gritos[random.randint(0, len(self._gritos) - 1)]
--- hola_mundo/el_mundo.py
+++ hola_mundo/el_mundo.py
@@ -1,2 +1,34 @@
-__author__ = 'Raul Gonzalez'
+"""Module to produce translations of 'mundo'.
 
+"""
+import os
+import random
+
+
+class ElMundo(object):
+    """Class that produces a random language version of 'mundo'."""
+
+    def __init__(self):
+        """Initialize Mundo from a resource file.
+
+        :return: Nothing is returned.
+        :rtype: None
+        """
+        current_dir = os.path.dirname(__file__)
+        mundo_file = os.path.join(current_dir, 'resources', 'mundo.txt')
+
+        with open(mundo_file) as f:
+            self._mundos = f.readlines()
+
+    def mundo(self, index=None):
+        """Method to retrieve a language version of 'mundo'.
+
+        :param index: An index key to retrieve a given 'mundo' translation.
+        :type index: int
+        :return: A language version of 'mundo'.
+        :rtype: str
+        """
+        if index is not None:
+            return self._mundo[index]
+        else:
+            return self._mundos[random.randint(0, len(self._mundos) - 1)]
