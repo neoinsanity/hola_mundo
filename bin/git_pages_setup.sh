3a29ff05c7bb02a24fa7b2c82d42aada3d1715b4
neoinsanity <mindbender@gmail.com>
2016-01-31 18:53:41 -0800

    Created the setup shell script for the generation of the gh-pages for documentation deployment.

--- bin/git_pages_setup.sh
+++ bin/git_pages_setup.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+# Create the clone.
+git clone https://github.com/neoinsanity/hola_mundo.git ./BUILD/gh_pages
+
+# Change into the work directory.
+ls ./BUILD/gh_pages
+cd ./BUILD/gh_pages
+
+# Initialize the gh_pages orphan branch.
+git checkout --orphan gh-pages
+git rm -rfv
+rm -v '.gitignore'
+echo "My Page" > index.html
+git add index.html
+git commit -a -m "First pages commit"
+git push origin gh-pages
+
+# Jump back to the root directory.
+cd -
+
+# Clean up behind the initialization.
+rm -rfv ./BUILD/gh_pages
+ls ./BUILD/gh_pages
